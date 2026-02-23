797a05877722a4086592a43d5abceef7fd1e65ca
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-02-23 15:53:18 +0530

    [Aniket] Refactor. UC4 - Added Yards and Centimeters as additional length units to the QuantityLength class

--- QuantityMeasurementAppTest/QuantityMeasurementAppTests.cs
+++ QuantityMeasurementAppTest/QuantityMeasurementAppTests.cs
@@ -5,115 +5,107 @@ namespace QuantityMeasurementAppTest
 {
     public class QuantityMeasurementAppTests
     {
-        // Yard to Yard equality
         [Test]
         public void testEquality_YardToYard_SameValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.YARDS);
-            Quantity q2 = new Quantity(1.0, LengthUnit.YARDS);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Yards);
+            Quantity q2 = new Quantity(1.0, LengthUnit.Yards);
 
-            Assert.AreEqual(q1, q2);
+            Assert.That(q1, Is.EqualTo(q2));
         }
 
         [Test]
         public void testEquality_YardToYard_DifferentValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.YARDS);
-            Quantity q2 = new Quantity(2.0, LengthUnit.YARDS);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Yards);
+            Quantity q2 = new Quantity(2.0, LengthUnit.Yards);
 
-            Assert.AreNotEqual(q1, q2);
+            Assert.That(q1, Is.Not.EqualTo(q2));
         }
 
-        // Yard to Feet
         [Test]
         public void testEquality_YardToFeet_EquivalentValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.YARDS);
-            Quantity q2 = new Quantity(3.0, LengthUnit.FEET);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Yards);
+            Quantity q2 = new Quantity(3.0, LengthUnit.Feet);
 
-            Assert.AreEqual(q1, q2);
+            Assert.That(q1, Is.EqualTo(q2));
         }
 
         [Test]
         public void testEquality_FeetToYard_EquivalentValue()
         {
-            Quantity q1 = new Quantity(3.0, LengthUnit.FEET);
-            Quantity q2 = new Quantity(1.0, LengthUnit.YARDS);
+            Quantity q1 = new Quantity(3.0, LengthUnit.Feet);
+            Quantity q2 = new Quantity(1.0, LengthUnit.Yards);
 
-            Assert.AreEqual(q1, q2);
+            Assert.That(q1, Is.EqualTo(q2));
         }
 
-        // Yard to Inches
         [Test]
         public void testEquality_YardToInches_EquivalentValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.YARDS);
-            Quantity q2 = new Quantity(36.0, LengthUnit.INCHES);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Yards);
+            Quantity q2 = new Quantity(36.0, LengthUnit.Inches);
 
-            Assert.AreEqual(q1, q2);
+            Assert.That(q1, Is.EqualTo(q2));
         }
 
-        // Centimeters to Inches
         [Test]
         public void testEquality_CentimetersToInches_EquivalentValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.CENTIMETERS);
-            Quantity q2 = new Quantity(0.393701, LengthUnit.INCHES);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Centimeters);
+            Quantity q2 = new Quantity(0.393701, LengthUnit.Inches);
 
-            Assert.AreEqual(q1, q2);
+            Assert.That(q1, Is.EqualTo(q2));
         }
 
         [Test]
         public void testEquality_CentimetersToFeet_NonEquivalentValue()
         {
-            Quantity q1 = new Quantity(1.0, LengthUnit.CENTIMETERS);
-            Quantity q2 = new Quantity(1.0, LengthUnit.FEET);
+            Quantity q1 = new Quantity(1.0, LengthUnit.Centimeters);
+            Quantity q2 = new Quantity(1.0, LengthUnit.Feet);
 
-            Assert.AreNotEqual(q1, q2);
+            Assert.That(q1, Is.Not.EqualTo(q2));
         }
 
-        // Transitive property
         [Test]
         public void testEquality_MultiUnit_TransitiveProperty()
         {
-            Quantity yard = new Quantity(1.0, LengthUnit.YARDS);
-            Quantity feet = new Quantity(3.0, LengthUnit.FEET);
-            Quantity inches = new Quantity(36.0, LengthUnit.INCHES);
+            Quantity yard = new Quantity(1.0, LengthUnit.Yards);
+            Quantity feet = new Quantity(3.0, LengthUnit.Feet);
+            Quantity inches = new Quantity(36.0, LengthUnit.Inches);
 
-            Assert.AreEqual(yard, feet);
-            Assert.AreEqual(feet, inches);
-            Assert.AreEqual(yard, inches);
+            Assert.That(yard, Is.EqualTo(feet));
+            Assert.That(feet, Is.EqualTo(inches));
+            Assert.That(yard, Is.EqualTo(inches));
         }
 
-        // Reflexive property
         [Test]
         public void testEquality_SameReference()
         {
-            Quantity q = new Quantity(2.0, LengthUnit.YARDS);
+            Quantity q = new Quantity(2.0, LengthUnit.Yards);
 
-            Assert.AreEqual(q, q);
+            Assert.That(q, Is.EqualTo(q));
         }
 
-        // Null comparison
         [Test]
         public void testEquality_NullComparison()
         {
-            Quantity q = new Quantity(2.0, LengthUnit.YARDS);
+            Quantity q = new Quantity(2.0, LengthUnit.Yards);
 
-            Assert.AreNotEqual(q, null);
+            Assert.That(q, Is.Not.EqualTo(null));
         }
 
-        // Complex scenario
         [Test]
         public void testEquality_AllUnits_ComplexScenario()
         {
-            Quantity yard = new Quantity(2.0, LengthUnit.YARDS);
-            Quantity feet = new Quantity(6.0, LengthUnit.FEET);
-            Quantity inches = new Quantity(72.0, LengthUnit.INCHES);
+            Quantity yard = new Quantity(2.0, LengthUnit.Yards);
+            Quantity feet = new Quantity(6.0, LengthUnit.Feet);
+            Quantity inches = new Quantity(72.0, LengthUnit.Inches);
 
-            Assert.AreEqual(yard, feet);
-            Assert.AreEqual(feet, inches);
-            Assert.AreEqual(yard, inches);
+            Assert.That(yard, Is.EqualTo(feet));
+            Assert.That(feet, Is.EqualTo(inches));
+            Assert.That(yard, Is.EqualTo(inches));
         }
     }
 }
